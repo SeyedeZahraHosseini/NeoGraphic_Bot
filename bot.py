f893abc0cc5d88e6e496ef1491e5756541f970aa
zahraHosseini246113 <zahrahosseini.13841384@gmail.com>
2026-08-28 11:16:19 +0330

    changes

--- bot.py
+++ bot.py
@@ -14,8 +14,9 @@ from moviepy import *
 
 global back_color
 apihelper.API_URL = "https://tapi.bale.ai/bot{0}/{1}"
-Token="603792:ZRk2hUFHWJ3jXuA8aKv3uSiga7SUmXBulq0"
-bot = telebot.TeleBot(Token)
+
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
 
 # region main_menu
 
